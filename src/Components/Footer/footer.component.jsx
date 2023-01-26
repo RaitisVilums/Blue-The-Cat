6a77a13820ef24df6d5849155aa89cc85dea807a
Raitis <96236875+RaitisVilums@users.noreply.github.com>
2023-01-26 11:37:09 +0200

    footer.component.jsx

--- src/Components/Footer/footer.component.jsx
+++ src/Components/Footer/footer.component.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-credentials">
-        <a href="#home">Go up</a>
+        <a href="#navigation">Go up</a>
         <h1>
           Made by{" "}
           <a href="https://github.com/RaitisVilums" target="_blank">
             Raitis Vilums
           </a>{" "}
         </h1>
-        <a href="#home">Go up</a>
+        <a href="#navigation">Go up</a>
       </div>
     </footer>
   );
